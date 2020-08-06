c005f7978c9138e1c92c45e930be0078052316e5
Hamza <hamza.eddouibi@gmail.com>
2020-08-06 17:32:53 +0200

    create error handlers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 
 const indexRouter = require('./routes/index')
@@ -15,8 +16,20 @@ server.use(bodyParser.json())
 
 server.use('/', indexRouter)
 server.use('/api', apiRouter)
+server.use(methodOverride())
+server.use(logErrors)
+server.use(errorHandler)
+
 
 
 server.listen(port, () => {
     console.log("xatkit-nlp.js-server running on port", port)
 })
+
+function logErrors (err, req, res, next) {
+    console.error(err.stack)
+    next(err)
+}
+function errorHandler (err, req, res, next) {
+    res.status(err.status).json({ message: err.message })
+}
